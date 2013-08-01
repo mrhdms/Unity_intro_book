efd2694d03da993605fbf2c2c783458c4acfb69a
mrhdms <mrhdms@gmail.com>
2013-08-01 21:36:56 +0900

    4-12トラップの点滅

---
+++ chapter4_maze/Assets/FlickerEffect.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlickerEffect : MonoBehaviour {
+	
+	private Color originalColor;
+
+	// Use this for initialization
+	void Start () {
+		originalColor = renderer.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float level = Mathf.Abs(Mathf.Sin(Time.time * 20));
+		renderer.material.color = originalColor * level;
+	}
+}
