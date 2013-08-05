c690e6eb78a5e3ef6045d10ac95f4e136ca20214
mrhdms <mrhdms@gmail.com>
2013-08-06 03:52:35 +0900

    chapter 5  list 5-1

---
+++ chapter5_block_shooter/Assets/Gun.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+	
+	public GameObject bulletPrefub;
+	public float initialVelocity; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			GameObject bullet = (GameObject)Instantiate(bulletPrefub, transform.position, transform.rotation);
+			Vector3 direction = transform.forward;
+			bullet.rigidbody.velocity = direction * initialVelocity;
+		}
+	}
+}
