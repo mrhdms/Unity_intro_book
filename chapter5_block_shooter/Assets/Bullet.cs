0ef90f7c3aec10fc9c4bed0b714031c3cc52ed77
mrhdms <mrhdms@gmail.com>
2013-08-06 04:08:33 +0900

    chapter5  list 5-3

---
+++ chapter5_block_shooter/Assets/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter (Collision collisionInfo) {
+		if (collisionInfo.gameObject.tag == "Box") {
+			Destroy(collisionInfo.gameObject);
+		}
+		Destroy(gameObject);
+	}
+}
