468f2303308efd2f08b824c253a7cd9a413a856b
mrhdms <mrhdms@gmail.com>
2013-08-08 03:44:31 +0900

    chapter 5  list 5-9

---
+++ chapter5_block_shooter/Assets/BoxGenerator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxGenerator : MonoBehaviour {
+	
+	public float interval;
+	public GameObject redBoxPrefab;
+	public GameObject blueBoxPrefab;
+	
+	private bool nextIsRed;
+	private float timer;
+
+	// Use this for initialization
+	void Start () {
+		nextIsRed = true;
+		timer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if ( timer < 0.0f ) {
+			GameObject prefab = nextIsRed ? redBoxPrefab : blueBoxPrefab;
+			Instantiate( prefab, transform.position, transform.rotation );
+			
+			timer = interval;
+			nextIsRed = !nextIsRed;
+		}
+	}
+}
